ba52884c8871867f93e785b965c0a1312a455498
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2025-10-29 14:32:37 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+/*
+ *  Copyright (c) 2023 Metaform Systems, Inc.
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Metaform Systems, Inc. - initial API and implementation
+ *
+ */
+
+import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+import com.github.jengelman.gradle.plugins.shadow.ShadowJavaPlugin
+
+plugins {
+    `java-library`
+    id("com.bmuschko.docker-remote-api") version "9.4.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    alias(libs.plugins.edc.build)
+}
+
+buildscript {
+    dependencies {
+        val version: String by project
+        classpath("org.eclipse.edc.autodoc:org.eclipse.edc.autodoc.gradle.plugin:$version")
+    }
+}
+
+val edcBuildId = libs.plugins.edc.build.get().pluginId
+
+allprojects {
+    apply(plugin = edcBuildId)
+    apply(plugin = "org.eclipse.edc.autodoc")
+
+    // configure which version of the annotation processor to use. defaults to the same version as the plugin
+    configure<org.eclipse.edc.plugins.autodoc.AutodocExtension> {
+        outputDirectory.set(project.layout.buildDirectory.asFile)
+    }
+}
+subprojects {
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.github.johnrengelman.shadow") &&
+            file("${project.projectDir}/src/main/docker/Dockerfile").exists()
+        ) {
+
+            //actually apply the plugin to the (sub-)project
+            apply(plugin = "com.bmuschko.docker-remote-api")
+            // configure the "dockerize" task
+            val dockerTask: DockerBuildImage = tasks.create("dockerize", DockerBuildImage::class) {
+                val dockerContextDir = project.projectDir
+                dockerFile.set(file("$dockerContextDir/src/main/docker/Dockerfile"))
+                images.add("${project.name}:${project.version}")
+
+                //images.add("${project.name}:latest")
+                // specify platform with the -Dplatform flag:
+                if (System.getProperty("platform") != null)
+                    platform.set(System.getProperty("platform"))
+                buildArgs.put("JAR", "build/libs/${project.name}.jar")
+                inputDir.set(file(dockerContextDir))
+            }
+            // make sure  always runs after "dockerize" and after "copyOtel"
+            dockerTask.dependsOn(tasks.named(ShadowJavaPlugin.SHADOW_JAR_TASK_NAME))
+        }
+    }
+}
---
+++ launchers/controlplane/build.gradle.kts
@@ -0,0 +1,62 @@
+/*
+ *  Copyright (c) 2025 Metaform Systems, Inc.
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Metaform Systems, Inc. - initial API and implementation
+ *
+ */
+
+import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+import com.github.jengelman.gradle.plugins.shadow.ShadowJavaPlugin
+import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.exclude
+
+plugins {
+    id("application")
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.docker)
+}
+
+dependencies {
+    runtimeOnly(libs.edcv.core.connector)
+    runtimeOnly(libs.edcv.core.negotiationmanager)
+    runtimeOnly(libs.edcv.core.transfermanager)
+    runtimeOnly(libs.edcv.banner)
+    runtimeOnly(libs.edcv.protocols.dsp)
+    runtimeOnly(libs.edc.core.connector)
+    runtimeOnly(libs.edc.core.runtime)
+    runtimeOnly(libs.edc.core.token)
+    runtimeOnly(libs.edc.core.jersey)
+    runtimeOnly(libs.edc.core.jetty)
+    runtimeOnly(libs.edc.api.observability)
+    runtimeOnly(libs.bundles.dcp)
+    runtimeOnly(libs.edc.core.controlplane) {
+        exclude("org.eclipse.edc", "control-plane-contract-manager")
+        exclude("org.eclipse.edc", "control-plane-transfer-manager")
+    }
+    runtimeOnly(libs.edc.core.dataplane.selector)
+    runtimeOnly(libs.edc.core.dataplane.signaling.client)
+    runtimeOnly(libs.edc.core.dataplane.signaling.transfer)
+
+    runtimeOnly(libs.edc.vault.hashicorp)
+    runtimeOnly(libs.edc.bom.controlplane.sql)
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    exclude("**/pom.properties", "**/pom.xm")
+    mergeServiceFiles()
+    archiveFileName.set("${project.name}.jar")
+}
+
+application {
+    mainClass.set("org.eclipse.edc.boot.system.runtime.BaseRuntime")
+}
+
+edcBuild {
+    publish.set(false)
+}
